aa625cb5cdd49438d231fc6a0909cd0787e29e04
Beatriz Cortes <bettycortes@Beatrizs-MacBook-Pro.local>
2019-11-27 15:25:49 -0600

    adding msg to test wasm

--- src/App.js
+++ src/App.js
@@ -41,6 +41,10 @@ class App extends Component {
     };
   }
 
+  bigComputation = () => {
+    alert("Big computation in JavaScript");
+  };
+
   handleOpenDescription = taskId => {
     let newTasks = Object.assign([], this.state.tasks);
     newTasks[taskId - 1].showDescription = !newTasks[taskId - 1]
@@ -52,6 +56,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <h1>To Do</h1>
+        <button onClick={this.bigComputation}>Run Computation</button>
         <List
           tasks={this.state.tasks}
           onOpen={this.handleOpenDescription}
